f28c1fd8bb28454970ecef93dd71b1a6d5a00f1f
tutamahesh <85124095+tutamahesh@users.noreply.github.com>
2024-08-28 11:04:54 +0530

    OAuthServer

--- src/main/java/com/spring/oauthserver/OAuthServerApplication.java
+++ src/main/java/com/spring/oauthserver/OAuthServerApplication.java
@@ -1,16 +1,14 @@
 package com.spring.oauthserver;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+
 
 @SpringBootApplication
 public class OAuthServerApplication {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder()
-		.profiles("DEV")
-		.sources(OAuthServerApplication.class)
-		.run(args);
+		SpringApplication.run(OAuthServerApplication.class, args);
 	}
 
 }
